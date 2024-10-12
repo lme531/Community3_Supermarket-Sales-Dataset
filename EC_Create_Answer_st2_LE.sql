c44fdf0d7de9637cb2900dd1afcc118d0adaac64
Lamidi Eleshin <lme531@ensign.edu>
2024-10-12 19:13:25 -0400

    Update EC_Create_Answer_st2_LE.sql

--- EC_Create_Answer_st2_LE.sql
+++ EC_Create_Answer_st2_LE.sql
@@ -1,8 +1,6 @@
-
 --Begin creating an answer.
 ------------------------------------------------------------------------------------------------
 --Q1:What is the highiest product line in the supermarket?
 
 --A1:The question will be answered for us  by T-SQL server........
-
 -------------------------------------------------------------------------------------------------
